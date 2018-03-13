20c74e3b929a1c3e9d0fc86e2777b344d8be81fc
Portablejim <portablejim@jamesmckay.id.au>
2018-03-14 01:00:10 +1100

    Try to debug preseed

--- scripts/vultr-stuff.bash
+++ scripts/vultr-stuff.bash
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-NEWHOSTNAME=$(wget -O - http://169.254.169.254/v1/hostname -q) && hostnamectl set-hostname $NEWHOSTNAME
+#NEWHOSTNAME=$(wget -O - http://169.254.169.254/v1/hostname -q) && hostnamectl set-hostname $NEWHOSTNAME
+NEWHOSTNAME=$(wget -O - http://169.254.169.254/v1/hostname -q) && echo $hostname > /etc/hostname
