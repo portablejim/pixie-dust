d139fb34e56dc0bd649620411f99883dd3594136
Portablejim <portablejim@jamesmckay.id.au>
2018-03-13 17:14:52 +1100

    Try to debug preseed

--- scripts/debian-stuff.bash
+++ scripts/debian-stuff.bash
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-cd /root;
-/bin/mkdir .ssh;
-/bin/chmod 0700 .ssh;
-echo 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILxh0ABdbGFSn4Kq8z2RICVxVZYZn0Nw8GW6wYy85A6V james@skeleton-windows' >> .ssh/authorized_keys;
-echo 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPcRUSo5AStg2sW+EooUv0t1QlCqjgAohWpxrhDWKF3w james@skeleton' >> .ssh/authorized_keys;
-/bin/chmod 0600 .ssh/authorized_keys;
-/bin/systemctl enable ssh;
+/bin/mkdir /root/.ssh || return 10;
+/bin/chmod 0700 /root/.ssh || return 11;
+echo 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILxh0ABdbGFSn4Kq8z2RICVxVZYZn0Nw8GW6wYy85A6V james@skeleton-windows' >> /root/.ssh/authorized_keys || return 12;
+echo 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPcRUSo5AStg2sW+EooUv0t1QlCqjgAohWpxrhDWKF3w james@skeleton' >> /root/.ssh/authorized_keys || return 13;
+/bin/chmod 0600 /root/.ssh/authorized_keys || return 14;
+/bin/systemctl enable ssh || return 15;
 
 #/usr/bin/passwd -l root
 
